63b5d8bd8c0a458390d1c94a6d6f029e3cfc66f4
Arsh Ahmad <42836035+arshahmad@users.noreply.github.com>
2022-04-22 22:42:36 +0530

    Time: 84 ms (52.04%), Space: 44.5 MB (42.34%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -3,7 +3,8 @@
  * @return {number}
  */
 var mostWordsFound = function(sentences) {
-    return Math.max.apply(null,  sentences.map(s => {
+    const l = sentences.map(s => {
         return s.split(' ').length
-    }));
+    });
+    return Math.max.apply(null, l);
 };
