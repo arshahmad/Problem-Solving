91d9bd45315bc486029a13f52912cafb1fc5781d
Arsh Ahmad <42836035+arshahmad@users.noreply.github.com>
2022-04-27 22:40:31 +0530

    Time: 82 ms (73.91%), Space: 45.1 MB (31.98%) - LeetHub

---
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+const cellsInRange = (s) => {
+  let letters = [s.charCodeAt(0), s.charCodeAt(3)];
+  let numbers = [Number(s[1]), Number(s[4])];
+  let res = [];
+
+  for (let i = letters[0]; i <= letters[1]; i++) {
+    for (let j = numbers[0]; j <= numbers[1]; j++) {
+      let cell = String.fromCharCode(i) + j;
+      res.push(cell);
+    }
+  }
+
+  return res;
+};
