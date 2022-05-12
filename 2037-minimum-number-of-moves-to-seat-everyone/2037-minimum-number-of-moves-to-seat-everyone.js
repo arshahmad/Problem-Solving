44a0a8558fa422b11f0ca5b833bfa41219686f08
Arsh Ahmad <42836035+arshahmad@users.noreply.github.com>
2022-05-12 22:49:11 +0530

    Time: 85 ms (59.17%), Space: 44.5 MB (55.00%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} seats
+ * @param {number[]} students
+ * @return {number}
+ */
+var minMovesToSeat = function(seats, students) {
+    let total = 0
+    seats=seats.sort((a,b)=>a-b)    
+    students=students.sort((a,b)=>a-b)
+    for(let i=0;i<seats.length;i++)
+        total+=Math.abs(seats[i]-students[i])
+    return total
+};
